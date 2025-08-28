287a31c1a4cd6602f35d1916c87aa04f41016759
Natali Schers <schersnatali@gmail.com>
2025-08-28 18:27:21 -0300

    feat: initialize React project with Vite and add basic styles

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components"
+import GlobalStyle from "./components/GlobalStyle";
+
+const GradientBackground = styled.div`
+    background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
+    width: 100%;
+    height: 100vh;
+`;
+
+function App() {
+  return (
+    <GradientBackground>
+
+    </GradientBackground>
+  )
+}
+
+export default App
